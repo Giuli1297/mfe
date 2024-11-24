1c2ee25481e657f0e96a7227ceaf7cd15f61f2aa
Giuliano <giuliano.gonzalez@edge.com.py>
2024-11-24 19:27:10 -0300

    Dashboard added

--- container/src/app.js
+++ container/src/app.js
@@ -1,22 +1,33 @@
-import React, {lazy, Suspense, useState} from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React, {lazy, Suspense, useState, useEffect} from "react";
+import { BrowserRouter, Route, Switch, Redirect, Router } from "react-router-dom";
 import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
+import {createBrowserHistory} from "history";
 
 import Header from "./components/Header";
 import ProgressBar from "./components/ProgressBar";
 
 const MarketingApp = lazy(() => import("./components/MarketingApp"));
 const AuthApp = lazy(() => import("./components/AuthApp"));
+const DashboardApp = lazy(() => import("./components/DashboardApp"));
 
 const generateClassName = createGenerateClassName({
     productionPrefix: "co"
 });
 
+const history = createBrowserHistory();
+
 export default () => {
     const [isSignedIn, setIsSignedIn] = useState(false);
+
+    useEffect(()=>{
+        if (isSignedIn){
+            history.push("/dashboard");
+        }
+    }, [isSignedIn]);
+
     return (
         <StylesProvider generateClassName={generateClassName}>
-            <BrowserRouter>
+            <Router history={history}>
                 <div>
                     <Header isSignIn={isSignedIn} onSignOut={()=>{setIsSignedIn(false)}}/>
                     <Suspense fallback={<ProgressBar/>}>
@@ -24,11 +35,15 @@ export default () => {
                             <Route path="/auth">
                                 <AuthApp onSignIn={() => setIsSignedIn(true)} />
                             </Route>
+                            <Route path="/dashboard">
+                                {!isSignedIn && <Redirect to="/" />}
+                                <DashboardApp />
+                            </Route>
                             <Route path="/" component={MarketingApp} />
                         </Switch>
                     </Suspense>
                 </div>
-            </BrowserRouter>
+            </Router>
         </StylesProvider>
     );
 };
